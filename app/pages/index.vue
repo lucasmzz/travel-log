0b7aea0a490b8cb6636c4154fbe2b66d916a5b4a
Lucas Mazzutti <lucasderh@gmail.com>
2025-07-29 16:36:35 -0300

    rm

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,3 @@
-<script lang="ts" setup>
-const { data } = await useFetch("/api/locations");
-</script>
-
 <template>
   <div
     class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4"
